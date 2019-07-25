0a0a0f11d55284d4f1a97dfcf3ad21cf1b951d4b
ryoji <ryojikitanodev@gmail.com>
2019-07-25 16:56:43 -0300

    crud

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -18,15 +18,16 @@
         <div class="row">
             <div class="col-lg-offset-3 col-lg-6">
             <div class="card-header border='1'" >
-  <div class="card-body">
-    <h5 class="card-title">ajax to do list<a href="#" class="pull-right" data-toggle="modal" data-target="#exampleModal"> <i class="fa fa-plus"></i></a></h5>
+  <div class="card-body" id="items">
+    <h5 class="card-title">ajax to do list<a href="#" id="addnovo" class="pull-right"  data-toggle="modal" data-target="#exampleModal"> <i class="fa fa-plus"></i></a></h5>
     <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
     <ul class="list-group">
-  <li class="list-group-item ourItem" data-toggle="modal" data-target="#exampleModal">Cras justo odio</li>
-  <li class="list-group-item ourItem" data-toggle="modal" data-target="#exampleModal">Dapibus ac facilisis in</li>
-  <li class="list-group-item ourItem" data-toggle="modal" data-target="#exampleModal">Morbi leo risus</li>
-  <li class="list-group-item ourItem" data-toggle="modal" data-target="#exampleModal">Porta ac consectetur ac</li>
-  <li class="list-group-item ourItem" data-toggle="modal" data-target="#exampleModal">Vestibulum at eros</li>
+    @foreach ($items as $item)
+    <li class="list-group-item ourItem" data-toggle="modal" data-target="#exampleModal">
+    {{$item->item}}
+    <input type="hidden" id="itemId" value="{{$item->id}}">
+    </li>
+    @endforeach
 </ul>
     <p class="card-text"></p>
 
@@ -47,13 +48,14 @@
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
+      <input type="hidden" id="id">
       <div class="modal-body">
         <p><input type="text" placeholder="escreva a tarefa" id="additem" class="form-control"></p>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-warning" id = "delete" data-dismiss="modal" style="display: none">delete</button>
-        <button type="button" class="btn btn-primary" id="salvar" style="display: none">Save changes</button>
-        <button type="button" class="btn btn-primary" id="adicionar">Adicionar</button>
+        <button type="button" class="btn btn-primary" id="salvar" data-dismiss="modal" style="display: none">Save changes</button>
+        <button type="button" class="btn btn-primary" data-dismiss="modal" id="adicionar">Adicionar</button>
       </div>
     </div>
   </div>
@@ -63,24 +65,73 @@
     </div>
 
     
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    {{csrf_field()}}
+    <script
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 <script>
+
+
     $(document).ready(function() {
-        $('.ourItem').each(function(){
-            $(this).click(function(event){
+      $(document).on('click', ' .ourItem', function(event){
+       event.preventDefault();
+           
                 var text = $(this).text();
+                var id = $(this).find('#itemId').val();
                 $('#title').text('Editar Item');
                 $("#additem").val(text);
                 $("#delete").show('400');
                 $("#salvar").show('400');
                 $("#adicionar").hide('400');
+                $("#id").val(id);
                 
                 console.log(text);
-            });
+            
+        });
+
+        
+          
+
+        $(document).on('click', '#addnovo', function(event){
+          $('#title').text('Adicionar Item');
+          $('#additem').val('');
+          $('#delete').hide('400');
+          $('#salvar').hide('400');
+          $('#adicionar').show('400');
+        });
+        
+        $('#adicionar').click(function(event)
+        {
+          var text = $('#additem').val();
+          $.post('list', {'text': text,'_token':$('input[name=_token]').val()}, function(data){
+            console.log(data);
+            $("#items").load(location.href + ' #items');
+            
+          });
+          
+        });
+
+        $('#delete').click(function(){
+          var id = $("#id").val();
+          $.post('delete',{'id': id , '_token':$('input[name=_token').val()}, function(data){
+            $("#items").load(location.href + ' #items');
+            console.log(data)
+          });
+          
+        });
+
+        $('#salvar').click(function(){
+          var id = $("#id").val();
+          var value = $('#additem').val();
+          $.post('update',{'id': id , 'value': value ,'_token':$('input[name=_token').val()}, function(data){
+            $("#items").load(location.href + ' #items');
+            console.log(data)
+          });
+          
         });
     });
     </script>
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::get('/', function () {
 Route::get('products', function(){
  return view ('welcome');
 });
-Route::get('list', function(){
-    return view ('list');
-});
+Route::get('list', 'ListController@index');
+Route::post('list', 'ListController@create');
+Route::post('delete','ListController@delete');
+Route::post('update', 'ListController@update');
